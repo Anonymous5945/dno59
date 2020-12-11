ced27a8619426c16dab2412180e99bc2e26e85c8
Anonymous5945 <56733554+Anonymous5945@users.noreply.github.com>
2020-12-11 16:34:29 +0530

    Update Sort_forward.py

--- tobrot/Source/Sort_forward.py
+++ tobrot/Source/Sort_forward.py
@@ -1,1 +1,25 @@
+import logging
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+LOGGER = logging.getLogger(__name__)
 
+
+import os
+
+from tobrot import (
+    DOWNLOAD_LOCATION,
+    MAX_MESSAGE_LENGTH
+)
+
+import asyncio
+import time
+from pyrogram.types import (
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    ReplyKeyboardMarkup
+)
+
+async def sortfor_f(client, message):
