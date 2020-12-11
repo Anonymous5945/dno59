e9d929415c5c234a2ae8d245a9366c8e9ece972f
Anonymous5945 <56733554+Anonymous5945@users.noreply.github.com>
2020-12-11 17:02:36 +0530

    Update Sort_forward.py

--- tobrot/Source/Sort_forward.py
+++ tobrot/Source/Sort_forward.py
@@ -8,10 +8,11 @@ LOGGER = logging.getLogger(__name__)
 
 
 import os
-
+import re
 from tobrot import (
     DOWNLOAD_LOCATION,
-    MAX_MESSAGE_LENGTH
+    MAX_MESSAGE_LENGTH,
+    sort_id
 )
 
 import asyncio
@@ -23,3 +24,32 @@ from pyrogram.types import (
 )
 
 async def sortfor_f(client, message):
+    n=message.message_id
+    w=message.reply_to_message.message_id
+    user_id = message.chat.id
+    user_id = message.chat.id
+    a=[]
+    b=[]
+    for i in range(w, n):
+        u_id = int(i)
+        m = await client.get_messages(user_id, u_id)
+        if m.media and m.document and m.document.file_name.lower().endswith(".mkv"):
+         j = m.document.file_name
+         h = m.message_id
+         a.append(j)
+         b.append(h)
+    h = [(a[i], b[i]) for i in range(0, len(a))]
+    n = len(h) 
+    for i in range(n): 
+        for j in range(n-i-1):
+             if h[j][0] > h[j + 1][0]:
+                 h[j], h[j + 1] = h[j + 1], h[j]
+    name , doc = zip(*h)
+    for i,j in zip(name,doc):
+       for ut in sort_id:
+         await client.forward_messages(
+      chat_id=ut,
+      from_chat_id=message.chat.id,
+      message_ids = j,
+      as_copy=True)
+         await asyncio.sleep(3)
