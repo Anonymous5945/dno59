8da7b2aa7e4404ab7b202656a83b62fe296e647b
Anonymous5945 <56733554+Anonymous5945@users.noreply.github.com>
2020-12-11 18:50:23 +0530

    Update Sort_forward.py

--- tobrot/Source/Sort_forward.py
+++ tobrot/Source/Sort_forward.py
@@ -12,7 +12,8 @@ import re
 from tobrot import (
     DOWNLOAD_LOCATION,
     MAX_MESSAGE_LENGTH,
-    sort_id
+    sort_id1,
+    sort_id2
 )
 
 import asyncio
@@ -23,7 +24,7 @@ from pyrogram.types import (
     ReplyKeyboardMarkup
 )
 
-async def sortfor_f(client, message):
+async def sortfor1_f(client, message):
     n=message.message_id
     w=message.reply_to_message.message_id
     user_id = message.chat.id
@@ -61,8 +62,55 @@ async def sortfor_f(client, message):
     name , doc = zip(*h)
     for i,j in zip(name,doc):
      if i.lower().endswith(".mkv"):
-      await client.send_document(message.chat.id,j, caption= "<b>" + i + "\n\n@kdg_166  @korea_drama\n@kdg166_ongoing @kdgfiles\n\nMuxed English Subtitles\n\nPlay it via external player</b>")
-      await asyncio.sleep(3)
+      for ut in sort_id1:
+       await client.send_document(ut,j, caption= "<b>" + i + "\n\n@kdg_166  @korea_drama\n@kdg166_ongoing @kdgfiles\n\nMuxed English Subtitles\n\nPlay it via external player</b>")
+       await asyncio.sleep(3)
      if i.lower().endswith(".mp4"):
-      await client.send_document(message.chat.id,j, caption= "<b>" + i + "\n\n@kdg_166  @korea_drama\n@kdg166_ongoing @kdgfiles</b>")
-      await asyncio.sleep(3)
+      for ut in sort_id1:
+       await client.send_document(ut,j, caption= "<b>" + i + "\n\n@kdg_166  @korea_drama\n@kdg166_ongoing @kdgfiles</b>")
+       await asyncio.sleep(3)
+async def sortfor2_f(client, message):
+    n=message.message_id
+    w=message.reply_to_message.message_id
+    user_id = message.chat.id
+    user_id = message.chat.id
+    a=[]
+    b=[]
+    o=[]
+    p=[]
+    for i in range(w, n):
+        u_id = int(i)
+        m = await client.get_messages(user_id, u_id)
+        if m.media and m.document and m.document.file_name.lower().endswith(".mp4"):
+         j = m.document.file_name
+         h = m.document.file_id
+         a.append(j)
+         b.append(h)
+        if m.media and m.document and m.document.file_name.lower().endswith(".mkv"):
+         s = m.document.file_name
+         r = m.document.file_id
+         o.append(s)
+         p.append(r)
+    h = [(a[i], b[i]) for i in range(0, len(a))]
+    n = len(h) 
+    for i in range(n): 
+        for j in range(n-i-1):
+             if h[j][0] > h[j + 1][0]:
+                h[j], h[j + 1] = h[j + 1], h[j]
+    k = [(o[i], p[i]) for i in range(0, len(o))]
+    n = len(k) 
+    for i in range(n): 
+         for j in range(n-i-1):
+             if k[j][0] > k[j + 1][0]:
+                 K[j], k[j + 1] = k[j + 1], k[j]
+    h.extend(k)
+    name , doc = zip(*h)
+    for i,j in zip(name,doc):
+     if i.lower().endswith(".mkv"):
+      for ut in sort_id2:
+       await client.send_document(ut,j, caption= "<b>" + i + "\n\n@kdg_166  @korea_drama\n@kdg166_ongoing @kdgfiles\n\nMuxed English Subtitles\n\nPlay it via external player</b>")
+       await asyncio.sleep(3)
+     if i.lower().endswith(".mp4"):
+      for ut in sort_id2:
+       await client.send_document(ut,j, caption= "<b>" + i + "\n\n@kdg_166  @korea_drama\n@kdg166_ongoing @kdgfiles</b>")
+       await asyncio.sleep(3)
