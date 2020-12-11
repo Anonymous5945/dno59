032dcddadcc13e931d042cde6d55d33c36d2db57
Anonymous5945 <56733554+Anonymous5945@users.noreply.github.com>
2020-12-11 16:32:55 +0530

    Update autoforward.py

--- tobrot/Source/autoforward.py
+++ tobrot/Source/autoforward.py
@@ -1,1 +1,28 @@
 
+import logging
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+LOGGER = logging.getLogger(__name__)
+
+
+import os
+
+from tobrot import (
+    DOWNLOAD_LOCATION,
+    MAX_MESSAGE_LENGTH,
+    user_ids1,
+    user_ids2
+)
+
+import asyncio
+import time
+from pyrogram.types import (
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    ReplyKeyboardMarkup
+)
+
+async def kdg1_f(client, message):
